18c39627ff958ffbc800e8acce13bed945f772fb
Milind_77 <milinddhote77@gmail.com>
2021-07-30 10:52:11 +0530

    [Milind] Add:- Inform user if entered invalid Mood.

---
+++ MoodAnalyzerProblem/MoodAnalysisException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerProblem
+{
+    class MoodAnalysisException : Exception
+    {
+        public enum Errors { EMPTY }
+        /// <summary>
+        /// custom exception for mood analysis. display an empty message.
+        /// </summary>
+        public MoodAnalysisException()
+        {
+            Console.WriteLine(Errors.EMPTY);
+        }
+    }
+}
